acb61e55f922e80762bc4484c336ff3c026e7d33
K <sohosmb@gmail.com>
2021-10-11 13:28:02 +0300

    fix provisioners.tf

--- output.tf
+++ output.tf
@@ -1,8 +1,15 @@
-output "virtual_machines" {
+output "vm_static_ips" {
+
   value =  merge(zipmap(libvirt_domain.virt_machine.*.name, libvirt_domain.virt_machine.*.network_interface.0.addresses.0)
      )
 }
+/*
+output "vm_dynamic_ip" {
 
+  value =  merge(zipmap(libvirt_domain.virt_machine.*.name, libvirt_domain.virt_machine.*.network_interface.0.addresses.0)
+     )
+}
+*/
 output "storage_size" {
   value = libvirt_volume.volume-qcow2.*.size
 }
