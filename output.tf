c1a619f35ad1b888dccf8ee3625ec8f49fb3ea5c
K <sohosmb@gmail.com>
2021-10-11 15:54:18 +0300

    fix provisioners.tf

--- output.tf
+++ output.tf
@@ -1,13 +1,7 @@
 output "vm_static_ips" {
-  value =  merge(zipmap(libvirt_domain.virt_machine.*.name, libvirt_domain.virt_machine.*.network_interface.0.addresses.0))
+  value =  libvirt_domain.virt_machine.*.network_interface.0.addresses.0 ? merge(zipmap(libvirt_domain.virt_machine.*.name, libvirt_domain.virt_machine.*.network_interface.0.addresses.0)) : null
 }
-/*
-output "vm_dynamic_ip" {
 
-  value =  merge(zipmap(libvirt_domain.virt_machine.*.name, libvirt_domain.virt_machine.*.network_interface.0.addresses.0)
-     )
-}
-*/
 output "storage_size" {
   value = libvirt_volume.volume-qcow2.*.size
 }
